7be0468bb827bec0101c2f16b349dc6175a93b5c
Ary <aryclenio.barros@truckpad.com.br>
2021-02-06 11:49:39 -0300

    :zap: Improving await sound

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import { playSound } from "./oscilator";
 import { Note } from "octavian";
 import file from "./notes.json";
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("button").addEventListener("click", async () => {
   const mapNotes = file.notes.map((note) => {
     const { frequency } = new Note(note.chord);
     return { frequency, time: note.time };
   });
-  mapNotes.forEach((note, index) => {
-    setTimeout(function () {
-      playSound(note.frequency, note.time);
-    }, note.time);
-  });
+  for (const note of mapNotes) {
+    await playSound(note.frequency, note.time);
+  }
 });
--- scripts/oscilator.js
+++ scripts/oscilator.js
@@ -1,21 +1,23 @@
-const context = new AudioContext();
+export const playSound = async (freq, time) => {
+  await new Promise((res, rej) => {
+    const context = new AudioContext();
 
-export const playSound = (freq, time) => {
-  const oscillator = context.createOscillator();
-  const envelope = context.createGain();
-  const decayRate = time * 1000; // seconds
+    const oscillator = context.createOscillator();
+    const envelope = context.createGain();
+    const decayRate = time * 1000; // seconds
 
-  oscillator.frequency.value = freq;
-  oscillator.type = "sine";
-  envelope.gain.value = 1;
+    oscillator.frequency.value = freq;
+    oscillator.type = "sine";
+    envelope.gain.value = 1;
 
-  oscillator.connect(envelope);
-  envelope.connect(context.destination);
+    oscillator.connect(envelope);
+    envelope.connect(context.destination);
 
-  oscillator.start(context.currentTime);
-  envelope.gain.exponentialRampToValueAtTime(
-    0.001,
-    context.currentTime + decayRate
-  );
-  setTimeout(() => oscillator.stop(context.currentTime), decayRate);
+    oscillator.start(context.currentTime);
+    envelope.gain.exponentialRampToValueAtTime(
+      0.001,
+      context.currentTime + decayRate
+    );
+    setTimeout(() => (oscillator.stop(context.currentTime), res()), decayRate);
+  });
 };
