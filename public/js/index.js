98b6b01c4ba9dd97334a0b23a7b4bd9fadf8f9f5
Ary <aryclenio.barros@truckpad.com.br>
2021-02-18 10:30:10 -0300

    💄 Updating UI

--- public/js/index.js
+++ public/js/index.js
@@ -4,5 +4,6 @@ import file from "../out/notes.json";
 import { initSound } from "./sound";
 import { initMetronome } from "./metronome";
 
+document.getElementById("music-name").innerText = file.name;
 initMetronome(file.metronome);
 initSound(file.notes);
