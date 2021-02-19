dcf465f687ca7c47bd2fc8389062b82a73996b23
Santana22 <santanajnz@gmail.com>
2021-02-19 13:23:02 -0300

    tester.py doesn't work
    let keep trying

--- lib/test/tester.py
+++ lib/test/tester.py
@@ -1,14 +1,31 @@
 import os
+import subprocess
 
 #Teste dos arquivos sintaticamente corretos
 
 for _,_, arquivo in os.walk('./examples/pass'):
     #comando para compilar
-    print(arquivo)
+    #print(arquivo)
+    #call =(["MusicConverter", arquivo])
+
+    #r = subprocess.call(['java ./lib/MusicConverter'])
+    #if r == 'Um arquivo JSON foi gerado com sucesso.':
+      #  print(arquivo(0) - 'Compilado com sucesso!')
+   # else:
+     #   print(arquivo(0) - 'Não identificado')
+    
 
 
 #Teste dos arquivos sintaticamente errados
 
 for _,_, arquivo in os.walk('./examples/fail'):
     #comando para compilar
-    print(arquivo)
+   # call =(["MusicConverter", arquivo])
+    #print(arquivo)
+
+    #subprocess.check_output(['java MusicConverter', arquivo])
+    #return r
+    #if r == 'Um arquivo JSON foi gerado com sucesso.':
+     #   print(arquivo(0) - 'Compilado com sucesso!')
+    #else:
+      #  print(arquivo(0) - 'Não identificado')
